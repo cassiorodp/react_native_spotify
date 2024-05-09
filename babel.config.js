25bc5f49a5b2f5d1c7d3ef28cf655cd8dfc99fd4
Cássio Pereira <cassiorodp@gmail.com>
2024-05-09 13:02:45 -0300

    chore: Add test setup

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  presets: ['babel-preset-expo', '@babel/preset-typescript'],
+  plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
+};
---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.setup.js',
+  ],
+};
+
+export default config;
