43141b29fa5e3013438b99a72f03afabc8996766
Cássio Pereira <cassiorodp@gmail.com>
2024-05-12 20:28:59 -0300

    refactor: Add clearSecureStore and storeToken functions

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,6 @@
+import { config } from '@/env/config';
+import { add, fromUnixTime } from 'date-fns';
+import { TokenResponse } from 'expo-auth-session';
 import * as SecureStore from 'expo-secure-store';
 
 const secureStore = async (key: string, value: string) => {
@@ -8,4 +11,23 @@ const getSecureStore = (key: string) => {
   return SecureStore.getItem(key);
 };
 
-export { secureStore, getSecureStore };
+const clearSecureStore = async () => {
+  await SecureStore.deleteItemAsync(config.tokenStoreKey);
+  await SecureStore.deleteItemAsync(config.refreshTokenStoreKey);
+  await SecureStore.deleteItemAsync(config.tokenStoreExpiryKey);
+};
+
+const storeToken = async (tokenResponse: TokenResponse) => {
+  await secureStore(config.tokenStoreKey, tokenResponse.accessToken);
+
+  if (tokenResponse.refreshToken)
+    await secureStore(config.refreshTokenStoreKey, tokenResponse.refreshToken);
+  await secureStore(
+    config.tokenStoreExpiryKey,
+    add(fromUnixTime(tokenResponse.issuedAt), {
+      seconds: tokenResponse.expiresIn,
+    }).toISOString()
+  );
+};
+
+export { secureStore, getSecureStore, clearSecureStore, storeToken };
