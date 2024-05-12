9c020e1d8a1d451bd3cbd8651dacd577b027f739
Cássio Pereira <cassiorodp@gmail.com>
2024-05-12 20:28:38 -0300

    refactor: Update API client to handle token refresh

--- api/client.ts
+++ api/client.ts
@@ -1,6 +1,8 @@
-import { config as envConfig } from '@/env/config';
-import { getSecureStore } from '@/utils';
+import { config as envConfig, endpointConfig, discovery } from '@/env/config';
+import { getSecureStore, storeToken } from '@/utils';
 import axios from 'axios';
+import { isAfter } from 'date-fns';
+import { refreshAsync } from 'expo-auth-session';
 
 const axiosInstance = axios.create({
   baseURL: envConfig.baseUrl,
@@ -9,6 +11,18 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   async (config) => {
     const token = getSecureStore(envConfig.tokenStoreKey);
+    const refreshToken = getSecureStore(envConfig.refreshTokenStoreKey);
+    const expiresIn = getSecureStore(envConfig.tokenStoreExpiryKey);
+    if (refreshToken && expiresIn && isAfter(new Date(), new Date(expiresIn))) {
+      const response = await refreshAsync(
+        {
+          ...endpointConfig,
+          refreshToken,
+        },
+        discovery
+      );
+      await storeToken(response);
+    }
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
--- api/endpoints.ts
+++ api/endpoints.ts
@@ -2,6 +2,5 @@ import axiosInstance from './client';
 
 export const getUserData = async () => {
   const data = await axiosInstance.get('/v1/me');
-  console.log('🚀 ~ getUserData ~ data:', data);
   return data;
 };
