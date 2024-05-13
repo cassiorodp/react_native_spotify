17b503846fa37c129bd9b5b517894994f76ca956
Cássio Pereira <cassiorodp@gmail.com>
2024-05-13 20:08:27 -0300

    feat: Add loading indicator to User screen

--- app/(tabs)/user.tsx
+++ app/(tabs)/user.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import React from 'react';
 import { CustomText } from '@/components/CustomText';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -9,11 +9,13 @@ import { Button } from '@/components/Button';
 import { clearSecureStore } from '@/utils';
 import { router } from 'expo-router';
 import Image from '@/components/Image';
+import { useTheme } from '@react-navigation/native';
 
 export default function User() {
-  const { data } = useQuery(routeTags.user, getUserData);
+  const { data, isLoading } = useQuery(routeTags.user, getUserData);
   const userName = data?.data?.display_name;
   const profileImage = data?.data?.images[0].url;
+  const { colors } = useTheme();
 
   const handleLogout = async () => {
     await clearSecureStore();
@@ -23,9 +25,11 @@ export default function User() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.userContainer}>
-        <View style={styles.imageContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={colors.primary} />
+        ) : (
           <Image width={128} height={128} src={profileImage} />
-        </View>
+        )}
         <CustomText type="subtitle">{userName}</CustomText>
       </View>
 
@@ -45,12 +49,6 @@ const styles = StyleSheet.create({
     gap: 16,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  imageContainer: {
-    width: 128,
-    height: 128,
-    borderRadius: 128,
-    overflow: 'hidden',
   },
   image: {
     flex: 1,
