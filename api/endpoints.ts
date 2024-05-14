a62ea08b8a681f3e1b1cd6a6b35fae602d54eb69
Cássio Pereira <cassiorodp@gmail.com>
2024-05-14 00:03:42 -0300

    feat: Add getArtistsData function to endpoints.ts

--- api/endpoints.ts
+++ api/endpoints.ts
@@ -4,3 +4,19 @@ export const getUserData = async () => {
   const data = await axiosInstance.get('/v1/me');
   return data;
 };
+
+export const getArtistsData = async ({
+  limit = 10,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) => {
+  const data = await axiosInstance.get('/v1/me/top/artists', {
+    params: {
+      limit,
+      offset: page * limit,
+    },
+  });
+  return data;
+};
