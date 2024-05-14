be47eb282f218b4abc40be7dacba0ab73b9f4092
Cássio Pereira <cassiorodp@gmail.com>
2024-05-14 00:03:48 -0300

    feat: Add tabs screen to RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout() {
             }}
           >
             <Stack.Screen name="index" />
+            <Stack.Screen name="tabs" />
           </Stack>
         </QueryClientProvider>
       </ThemeProvider>
