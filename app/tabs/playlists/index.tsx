26d4ce96f61caf3da22cd2344fcc20a828d86744
Cássio Pereira <cassiorodp@gmail.com>
2024-05-14 23:34:53 -0300

    refactor: Rename Explore component to PlayList

--- app/tabs/playlists/index.tsx
+++ app/tabs/playlists/index.tsx
@@ -2,7 +2,7 @@ import SafeAreaViewWithPadding from '@/components/SafeAreaViewWithPadding';
 import UserHeader from '@/components/UserHeader';
 import UserPlaylists from '@/components/UserPlaylists';
 
-export default function Explore() {
+export default function PlayList() {
   return (
     <SafeAreaViewWithPadding>
       <UserHeader title="Minhas playlists" />
